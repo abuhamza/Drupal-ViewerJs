fa2a8459b90356b2c6e0b380c54d10998359108e
Mouhammed DIOP <mdiop@peopleinput.com>
2014-12-28 22:08:57 +0000

    Issue #2 from interactivejunky : Wrap all the code in (function ($) { })(jQuery); and use $ notation to make reference to jQuery.

--- js/viewerjs.js
+++ js/viewerjs.js
@@ -3,57 +3,60 @@
  * File previewer using ViewerJS Library.
  */
 
-/**
- * Center an element in the absolute middle of the screen.
- */
-jQuery.fn.viewerjsCenterElement = function () {
-  var paddingLeft = parseInt(jQuery(this).css('padding-left'), 10);
-  var paddingRight = parseInt(jQuery(this).css('padding-right'), 10);
-  var paddingTop = parseInt(jQuery(this).css('padding-top'), 10);
-  var paddingBottom = parseInt(jQuery(this).css('padding-bottom'), 10);
-  var borderLeft = parseInt(jQuery(this).css('borderLeftWidth'), 10);
-  var borderRight = parseInt(jQuery(this).css('borderRightWidth'), 10);
-  var borderTop = parseInt(jQuery(this).css('borderTopWidth'), 10);
-  var borderBottom = parseInt(jQuery(this).css('borderBottomWidth'), 10);
-  var left = parseInt(jQuery(window).width() / 2, 10) - parseInt((jQuery(this).width() + paddingLeft + paddingRight + borderLeft + borderRight) / 2, 10);
-  var top = parseInt(jQuery(window).height() / 2, 10) - parseInt((jQuery(this).height() + paddingBottom + paddingTop + borderBottom + borderTop) / 2, 10);
-  jQuery(this).css({left: left + "px",top: top + "px",position: "fixed"});
-  return jQuery(this);
-};
+(function ($) {
+  /**
+   * Center an element in the absolute middle of the screen.
+   */
+  $.fn.viewerjsCenterElement = function () {
+    var paddingLeft = parseInt($(this).css('padding-left'), 10);
+    var paddingRight = parseInt($(this).css('padding-right'), 10);
+    var paddingTop = parseInt($(this).css('padding-top'), 10);
+    var paddingBottom = parseInt($(this).css('padding-bottom'), 10);
+    var borderLeft = parseInt($(this).css('borderLeftWidth'), 10);
+    var borderRight = parseInt($(this).css('borderRightWidth'), 10);
+    var borderTop = parseInt($(this).css('borderTopWidth'), 10);
+    var borderBottom = parseInt($(this).css('borderBottomWidth'), 10);
+    var left = parseInt($(window).width() / 2, 10) - parseInt(($(this).width() + paddingLeft + paddingRight + borderLeft + borderRight) / 2, 10);
+    var top = parseInt($(window).height() / 2, 10) - parseInt(($(this).height() + paddingBottom + paddingTop + borderBottom + borderTop) / 2, 10);
+    $(this).css({left: left + "px", top: top + "px", position: "fixed"});
+    return $(this);
+  };
 
-jQuery.fn.viewerjsCreateOverlay = function () {
-  if (!jQuery(this).hasClass('viewerjs-processed')) {
-    jQuery(this).addClass('viewerjs-processed').click(function (e) {
-      e.preventDefault();
-      var id = jQuery(this).attr('data-viewerjs-overlay-id');
-      // Create the background.
-      var fog = jQuery('<div id="' + id + '-fog" class="viewerjs-overlay-fog"></div>');
-      fog.appendTo("body").css({
-        'top': '0px',
-        'left': '0px',
-        'width': jQuery('body').outerWidth(true) + 'px',
-        'height': jQuery('body').outerHeight(true) + 'px',
-        'position': 'fixed'
-      });
-      var overlay = jQuery('<div class="viewerjs-overlay-element"></div>').attr({
+  $.fn.viewerjsCreateOverlay = function () {
+    if (!$(this).hasClass('viewerjs-processed')) {
+      $(this).addClass('viewerjs-processed').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('data-viewerjs-overlay-id');
+        // Create the background.
+        var fog = $('<div id="' + id + '-fog" class="viewerjs-overlay-fog"></div>');
+        fog.appendTo("body").css({
+          'top': '0px',
+          'left': '0px',
+          'width': $('body').outerWidth(true) + 'px',
+          'height': $('body').outerHeight(true) + 'px',
+          'position': 'fixed'
+        });
+        var overlay = $('<div class="viewerjs-overlay-element"></div>').attr({
           id: id
         }).css({
-          width: jQuery(this).attr('data-viewerjs-overlay-width'),
-          height: jQuery(this).attr('data-viewerjs-overlay-height')
+          width: $(this).attr('data-viewerjs-overlay-width'),
+          height: $(this).attr('data-viewerjs-overlay-height')
         });
-      overlay.html('<iframe id="' + id + '-iframe" src="' + jQuery(this).attr('href') + '" width="' + jQuery(this).attr('data-viewerjs-overlay-width') + '" height="' + jQuery(this).attr('data-viewerjs-overlay-height') + '" allowfullscreen webkitallowfullscreen></iframe>');
-      overlay.appendTo("body").viewerjsCenterElement();
-      jQuery(fog).click(function(){
-        jQuery(fog).remove();
-        jQuery(overlay).remove();
+        overlay.html('<iframe id="' + id + '-iframe" src="' + $(this).attr('href') + '" width="' + $(this).attr('data-viewerjs-overlay-width') + '" height="' + $(this).attr('data-viewerjs-overlay-height') + '" allowfullscreen webkitallowfullscreen></iframe>');
+        overlay.appendTo("body").viewerjsCenterElement();
+        $(fog).click(function () {
+          $(fog).remove();
+          $(overlay).remove();
+        });
+        return overlay;
       });
-      return overlay;
-    });
-  }
-};
-/**/
+    }
+  };
 
-/**/
-jQuery(document).ready(function () {
-  jQuery("a.viewerjs-overlay").viewerjsCreateOverlay();
-});
+  Drupal.behaviors.viewerjs = {
+    attach: function (context, settings) {
+      $("a.viewerjs-overlay").viewerjsCreateOverlay();
+    }
+  };
+
+})(jQuery);
