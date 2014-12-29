549b6116a479ce0805fe0f5b916a6ab12b9036e7
Mouhammed DIOP <mdiop@peopleinput.com>
2014-12-29 19:50:04 +0000

    Add file doc comment End file with single new line character Add comma to the last multiline array item

--- viewerjs.tpl.php
+++ viewerjs.tpl.php
@@ -1,3 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for viewerjs_field_formatter_view
+ *
+ * Available variables:
+ * - $color : The class to display widget in built-in color.
+ * - $thumbnail : Thumbnail of file extension or image.
+ * - $icon : File type icon.
+ * - $filename : The filename or file description if exist.
+ * - $size : The size of the document.
+ * - $preview_link : Display link to preview document with ViewerJS.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_viewerjs()
+ * @see template_process()
+ */
+?>
+
 <div class="viewerjs-field-item">
   <div class="viewerjs-item-wrapper <?php print $color; ?>"
        style="<?php ($thumbnail) ? print $thumbnail : print ''; ?>">
